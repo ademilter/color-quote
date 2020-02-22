f80cd988a77ba9c5e3ff63743abdde894a0e4824
Adem ilter <ademilter@gmail.com>
2020-02-22 20:13:24 +0300

    add analytic

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { GA_TRACKING_ID } from '../lib/analytics'
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -10,8 +12,30 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
+        <Head lang="en">
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}');
+                  `
+            }}
+          />
+
           <title>Color Quote</title>
+
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+
           <link
             href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:700&display=swap&subset=latin-ext"
             rel="stylesheet"
